32c6e88da19bf0bbd1c1bc89bad55bb55f465bb0
Luis Gustavo <gustascrows@gmail.com>
2021-03-31 15:26:54 -0300

    Feat: Passando parametro com JSX

---
+++ Primeira aula/src/components/basics/Primeiro.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Primeiro(){
+    return (
+        <div>
+        <h2>Retorno</h2>
+        <p>Teste</p>
+        </div>
+    );
+}
--- Primeira aula/src/index.js
+++ Primeira aula/src/index.js
@@ -2,10 +2,12 @@ import './index.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Primeiro from './components/basics/Primeiro'
+import ComParametro from './components/basics/ComParametro';
 
 const el = document.getElementById('root');
 ReactDOM.render(
     <div>
        <Primeiro></Primeiro>
+       <ComParametro titulo={'Titulo'} subtitulo={'Subtitulo'}></ComParametro>
     </div>
     , el);
