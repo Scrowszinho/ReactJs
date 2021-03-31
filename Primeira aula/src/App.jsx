8887e30ed4e763a297bbe5dd16282c111f827859
Luis Gustavo <gustascrows@gmail.com>
2021-03-31 17:03:44 -0300

    Feat: Children component

--- Primeira aula/src/App.jsx
+++ Primeira aula/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Primeiro from './components/basics/Primeiro'
 import ComParametro from './components/basics/ComParametro';
+import Card from './components/layouts/Card';
+
 
 export default () => {
     return (
         <div id='app'>
-            <Primeiro primeiro={Math.random() * (100 - 0) + 0} segundo={Math.random() * (100 - 0) + 0}></Primeiro>
-            <ComParametro titulo={'Titulo'} subtitulo={'Subtitulo'}></ComParametro>
+            <Card titulo={'Teste'}>
+                <Primeiro primeiro={Math.random() * (100 - 0) + 0} segundo={Math.random() * (100 - 0) + 0}></Primeiro>
+            </Card>
+            <Card titulo={'Teste'}>
+                <ComParametro titulo={'Titulo'} subtitulo={'Subtitulo'}></ComParametro>
+            </Card>
+
         </div>
     );
 }
