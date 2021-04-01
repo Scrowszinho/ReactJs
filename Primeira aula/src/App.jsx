28d863c2c7f4d29cc6fcb9d21de4cf817756349b
Luis Gustavo <gustascrows@gmail.com>
2021-04-01 17:04:59 -0300

    Aula: Passando estilo por parametro

--- Primeira aula/src/App.jsx
+++ Primeira aula/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Primeiro from './components/basics/Primeiro'
 import ComParametro from './components/basics/ComParametro';
+import CoresParametro from './components/basics/CoresParametro';
 import Card from './components/layouts/Card';
 import './App.css';
 
@@ -12,8 +13,12 @@ export default () => {
             <Card titulo={'#01 - Números maiores'}>
                 <Primeiro primeiro={(Math.random() * (100 - 0) + 0).toFixed()} segundo={(Math.random() * (100 - 0) + 0).toFixed()}></Primeiro>
             </Card>
-            <Card titulo={'#02 - Com Parametro'}>
+            <Card titulo={'#02 - Com Parametro'} color={'#008'}>
                 <ComParametro titulo={'Titulo'} subtitulo={'Subtitulo'}></ComParametro>
+            </Card>
+
+            <Card titulo={'#03 - Cores com Parametro'}  color={'#0F0'}>
+                <CoresParametro color={'#0F0'}></CoresParametro>
             </Card>
             </div>
         </div>
--- Primeira aula/src/components/layouts/Card.jsx
+++ Primeira aula/src/components/layouts/Card.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import './Card.css';
 
 export default (props) =>{
+    const estilo = {
+        backgroundColor: props.color || '#F00',
+        borderColor: props.color || '#F00'
+    }
     return(
-        <div className='Card'>
+        <div className='Card' style={estilo}>
             <div className='Title'>{props.titulo}</div>
-            <div className='Content'>{props.children}</div>
+            <div className='Content' >{props.children}</div>
         </div>
     );
 }
