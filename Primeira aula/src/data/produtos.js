fd2ad205ccac9d85f8e9f1009d7ee5e9b039c93f
Luis Gustavo <gustascrows@gmail.com>
2021-04-06 17:47:00 -0300

    Aula: Produtos dos Exercicios de Repeticao

---
+++ Primeira aula/src/data/produtos.js
@@ -0,0 +1,7 @@
+export default [
+    {id: 1, name:'Chocolate', price: 1.5},
+    {id: 2, name:'Cerveja', price: 3.5},
+    {id: 3, name:'Pão', price: 0.5},
+    {id: 4, name:'Coca-Cola lata', price: 3.5},
+    {id: 5, name:'Bolacha', price: 2.5},
+];
