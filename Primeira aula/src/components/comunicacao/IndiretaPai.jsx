71b8005ed821fe854b84092f3e3f72a67c326a42
Luis Gustavo <gustascrows@gmail.com>
2021-04-08 16:45:51 -0300

    Aulas: Componente com estado

--- Primeira aula/src/components/comunicacao/IndiretaPai.jsx
+++ Primeira aula/src/components/comunicacao/IndiretaPai.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React,{useState} from 'react';
 import IndiretaFilho from './IndiretaFilho';
 
 export default props =>{
-    const nome = '?';
-    const numero = 0;
+   const[nome,setNome] = useState('?');
+   const[numero,setNumero] = useState(0);
     const fornecerInfo = (nome, numero) =>{
         console.log(nome,numero);
+        setNome(nome);
+        setNumero(numero);
     }
     return(
         <div>
-            <span>{nome} </span>
+            <span>{nome}</span>
             <span>{numero}</span>
             <IndiretaFilho onClick={fornecerInfo}></IndiretaFilho>
         </div>
