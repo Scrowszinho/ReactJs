6ad8235414ffc042b1c952eb50c5512befd64261
Luis Gustavo <gustascrows@gmail.com>
2021-04-14 17:06:12 -0300

    Aula: Componentes iniciais

---
+++ navegace/src/components/layouts/Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Content.css';
+const Content = props =>{
+ return(
+ <Main className='Content'>
+    Componente Content
+    </Main>
+ )
+}
+
+export default Content;
---
+++ navegace/src/views/examples/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const About = props =>{
+ return(
+ <div className='About'>
+   <h1>Componente About</h1>
+   <p>Sobre</p>
+    </div>
+ )
+}
+
+export default About;
