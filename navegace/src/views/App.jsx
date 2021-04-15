71538d680d1ca724736ccaae29ce0ddf5e1bee40
Luis Gustavo <gustascrows@gmail.com>
2021-04-15 16:20:53 -0300

    Aula: Passando o Router em volta dos componentes

--- navegace/src/views/App.jsx
+++ navegace/src/views/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import Menu from '../components/layouts/Menu';
 import Content from '../components/layouts/Content';
+import {BrowserRouter as Router} from 'react-router-dom';
 
-const App = props =>{
- return(
- <div className='App'>
-    <Menu />
-    <Content />
-    </div>
- )
+const App = props => {
+   return (
+      <div className='App'>
+         <Router>
+            <Menu />
+            <Content />
+         </Router>
+      </div>
+   )
 }
 
 export default App;
