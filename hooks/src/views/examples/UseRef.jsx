c9e64096a642082ae3ddbcf3e11940e9367a521e
Luis Gustavo <gustascrows@gmail.com>
2021-04-29 13:59:15 -0300

    Aula: UseRef aula 1

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React,{useState, useRef} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle';
 
 const UseRef = (props) => {
+    const[text, setText] = useState('');
+    const count = useRef(0);
+    count.current ++;
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+            <SectionTitle title='Exer #01' />
+            <div className="center">
+                <input type="text" className="input" value={text} onChange={e=>setText(e.target.value)} />
+               <div>
+                <span className="text">Valor: </span>
+                <span className="text">{text}</span>
+                <span className="text red">[{count.current}]</span>
+                </div>
+            </div>
         </div>
     )
 }
