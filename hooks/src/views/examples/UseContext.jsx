3ceae4583ba0313b658ddb78d50e05d57fe8e2b3
Luis Gustavo <gustascrows@gmail.com>
2021-04-30 13:20:05 -0300

    Aula: Aula de useContext 02

--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,15 +1,18 @@
 import React, { useContext } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import DataContext from '../../data/DataContext';
+import { AppContext } from '../../data/Store';
 
 const UseContext = (props) => {
     const context = useContext(DataContext);
-  function setNum(n){
-    context.setState({
-        ...context.state,
-        number: context.state.number + n
-    })
-  }
+    function setNum(n) {
+        context.setState({
+            ...context.state,
+            number: context.state.number + n
+        })
+    }
+
+    const { number, setNumber, text, setText } = useContext(AppContext);
     return (
         <div className="UseContext">
             <PageTitle
@@ -21,8 +24,22 @@ const UseContext = (props) => {
                 <span className="text">{context.state.number}</span>
             </div>
             <div>
-                <button className="btn" onClick={()=>setNum(+1)}>+1</button>
-                <button className="btn" onClick={()=>setNum(-1)}>-1</button>
+                <button className="btn" onClick={() => setNum(+1)}>+1</button>
+                <button className="btn" onClick={() => setNum(-1)}>-1</button>
+            </div>
+            <PageTitle title='Exercicio #02' />
+            <div className="center">
+                <span className="text">{number}</span>
+                <div>
+                    <button className="btn" onClick={() => setNumber(number - 1)}>
+                        -1
+                </button>
+                <button className="btn" onClick={() => setNumber(number + 1)}>
+                        +1
+                </button>
+                </div>
+                <span className="text">{text}</span>
+                <button className="btn" onClick={()=>setText('Texto teste')}>Trocar texto</button>
             </div>
         </div>
     )
