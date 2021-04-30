ff722537f2906f773dd2ffaa6d475a9f15f5c991
Luis Gustavo <gustascrows@gmail.com>
2021-04-30 16:52:28 -0300

    Aula: Componente do reducers

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,37 +1,7 @@
 import React, { useReducer, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
-
-const initialState = {
-    number: 0,
-    other: 'Seja bem vindo'
-}
-
-function reducer(state, action) {
-    switch (action.type) {
-        case 'add2':
-            return { ...state, number: state.number + 2 }
-            break;
-        case 'login':
-            return {...state, other:action.other}
-        break;
-        case 'multi':
-            return {...state, number: state.number * 7}
-            break;
-        case 'diviser':
-            return {...state, number:state.number/25}
-        break;
-        case 'parser':
-            return {...state, number: parseInt(state.number)}
-        break;
-        case 'setNum':
-            return {...state, number: action.number}
-        break;
-        default:
-            return state
-            break;
-    }
-}
-
+import {initialState, reducer} from '../../store';
+import {add2,multi} from '../../store/actions/';
 const UseReducer = (props) => {
     const [state, dispatch] = useReducer(reducer, initialState);
     const [tamanho, setTamanho] = useState( );
@@ -45,7 +15,7 @@ const UseReducer = (props) => {
             <span className="text">{state.number}</span>
                 <span className="text">{state.other}</span>
             <div>
-                <button className="btn" onClick={()=>dispatch({type:'add2'})}>+2</button>
+                <button className="btn" onClick={()=>add2(dispatch)}>+2</button>
                 <button className="btn" onClick={()=>dispatch({type:'login',other:'Gustavo'})}>Login</button>
                 <button className="btn" onClick={()=>dispatch({type:'multi'})}>x7</button>
                 <button className="btn" onClick={()=>dispatch({type:'diviser'})}>/25</button>
